d8de2619260faad5140b0660f650a3b0dc66d59f
101001000 <kike.delacalle.montilla@gmail.com>
2022-08-12 16:30:02 +0200

    WIP

--- src/mainscript.py
+++ src/mainscript.py
@@ -1,17 +1,108 @@
 import bpy
+import socket
 import array
 import gpu
 import json
 import subprocess
+import struct
 import sys
 import os
 import math
-import imageio
+import time
 import numpy as np
 from gpu_extras.presets import draw_texture_2d
 
 
 
+TCP_MESSAGE_MAXSIZE = 1024
+
+def write_message(msg, sock):
+    
+    message_str = json.dumps(msg, indent = 4) 
+    message_bytes = message_str.encode('utf-8')
+    
+    sock.sendall(message_bytes)
+    
+    if msg["data_size"] > 0:
+        sock.sendall(bytes(msg["data"]))
+
+
+def read_message(sock):
+            
+    message_bytes = sock.recv(TCP_MESSAGE_MAXSIZE)
+    message_str = message_bytes.decode("utf-8")
+    message_json = json.loads(message_str)
+    
+    if message_json["data_size"] > 0:
+        
+        message_json["data"] = bytearray()
+        amount_received = 0
+                
+        while amount_received < message_json["data_size"]:
+            data = sock.recv(8096)
+            amount_received += len(data)
+            message_json["data"] += data
+    
+    return message_json    
+    
+    
+
+def sceneTCP(scene_path):
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server_address = ('127.0.0.1', 5557)
+    print('connecting to {} port {}'.format(*server_address))
+    sock.connect(server_address)
+    print('connected!')
+
+    response_bytes = bytearray()
+
+    try:
+        
+        scene_load_msg = dict()
+        
+        scene_load_msg["type"] = "command"
+        scene_load_msg["msg"] = "--load_obj " + scene_path      
+        
+        write_message(scene_load_msg, sock)        
+
+    finally:
+        print('closing socket')
+        sock.close()
+
+
+def tcpTest():
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    server_address = ('127.0.0.1', 5557)
+    print('connecting to {} port {}'.format(*server_address))
+    sock.connect(server_address)
+
+    response_data = []
+    response_bytes = bytearray()
+
+
+    try:
+      
+        amount_received = 0
+        amount_expected = 1920*1080*4*4
+                
+        while amount_received < amount_expected:
+            data = sock.recv(8096)
+            amount_received += len(data)
+            response_bytes += data
+
+    finally:
+        print('closing socket')
+        sock.close()
+
+    # I will need to write a custom c module to break this bottleneck           
+    arr = np.frombuffer(response_bytes, dtype=np.single)    
+    arr = arr.reshape((1920*1080, 4))          
+    return arr 
+    
 
 class CustomRenderEngine(bpy.types.RenderEngine):
     # These three members are used by blender to set up the
@@ -35,122 +126,20 @@ class CustomRenderEngine(bpy.types.RenderEngine):
     # This is the method called by Blender for both final renders (F12) and
     # small preview for materials, world and lights.
     def render(self, depsgraph):
+                
         scene = depsgraph.scene
         scale = scene.render.resolution_percentage / 100.0
         self.size_x = int(scene.render.resolution_x * scale)
         self.size_y = int(scene.render.resolution_y * scale)
 
-        # Fill the render result with a flat color. The framebuffer is
-        # defined as a list of pixels, each pixel itself being a list of
-        # R,G,B,A values.
-        if self.is_preview:
-            color = [0.1, 0.2, 0.1, 1.0]
-        else:
-            color = [1, 0, 0, 1.0]
+        sceneTCP("C:\\Users\\Kike\\Desktop\\TFM\\scenes\\blenderTest")
 
-        pixel_count = self.size_x * self.size_y
-        rect = [color] * pixel_count
-                        
-        print("Starting rendering proccess")
-    
-        scene_object = dict()
-        
-        #Camera setup:
-        b_cam_loc = bpy.data.objects['Camera'].location
-        b_cam_rot = bpy.data.objects['Camera'].rotation_euler 
-        
-        scene_object['camera'] = dict()
-                
-        scene_object['camera']['xRes'] = scene.render.resolution_x
-        scene_object['camera']['yRes'] = scene.render.resolution_y
-        
-        scene_object['camera']['position'] = {'x':b_cam_loc.x,'y':b_cam_loc.z,'z':b_cam_loc.y}
-        scene_object['camera']['rotation'] = {'x': 90 - b_cam_rot.x / (math.pi/180), 'y':-b_cam_rot.z / (math.pi/180), 'z': -b_cam_rot.y / (math.pi/180)}
 
-        scene_object['camera']['focalLength'] = 0.05
-        scene_object['camera']['focusDistance'] = 0.69
-        scene_object['camera']['aperture'] = 2.8
-        scene_object['camera']['bokeh'] = "false"
-        
-        
-        #Hdri setup:
-        
-        scene_object['hdri'] = dict()
-          
-        if bpy.context.scene.world.node_tree.nodes.find('Environment Texture') != -1:
-            pass
-        else:
-            b_env_color = bpy.context.scene.world.node_tree.nodes['Background'].inputs[0].default_value
-            scene_object['hdri']['color'] = {'r':b_env_color[0], 'g':b_env_color[1], 'b':b_env_color[2]}        
-        
-        
-        objs = bpy.context.scene.objects
-        
-        rendering_path = "C:\\Users\\Kike\\Desktop\\TFM\\scenes\\blenderTest"
-        output_file_path = "C:\\Users\\Kike\\Desktop\\TFM\\repos\\ElevenRender\\output.png"
-        
-        proc = '"C:\\Users\\Kike\\Desktop\\TFM\\repos\\ElevenRender\\ElevenRender.exe" "' + rendering_path  + '" 50 "' + output_file_path + '"'
-
-        print(proc)
-
-        """
-
-        arr = np.fromiter([1.1], dtype=np.single)
-        bytedata = arr.tobytes()
-        print(arr)
-        print(bytedata)
-        print("\n byte data: ", bytedata[0], " - ", bytedata[1], " - ", bytedata[2], " - ", bytedata[3], "\n")
-
-        """
-
-        #pipe = subprocess.Popen(proc, shell=False, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=4096*4096*4*4)
-        
-        #print(pipe.communicate(input=b'one\ntwo\nthree\nfour\nfive\nsix\n')[0])
-        
-        """
-        
-        for obj in objs:
-        
-            if obj.name != "Camera":
-                for node in obj.active_material.node_tree.nodes:
-                    if node.type == 'TEX_IMAGE':
-                        print("Sending img", node.image)
-                        floatdata = np.fromiter(node.image.pixels, dtype=np.single)
-    
-                        
-                        bytedata = floatdata.tobytes()
-
-                        print(pipe.communicate(input=bytedata)[0])
-                        #print("first value: ", node.image.pixels[0], " and ", floatdata[0], "\n byte data: ", bytedata[0], " - ", bytedata[1], " - ", bytedata[2], " - ", bytedata[3], "\n")
-
-                        #for i in range(0, len(bytedata), chunksize): 
-                        #    print(pipe.communicate(input=bytedata[i:i+chunksize])[0])
-                        break
-                        
-                break   
-
-        """    
-
-        bpy.ops.export_scene.obj(filepath=rendering_path + "\\scene.obj", use_triangles=True, path_mode='ABSOLUTE')            
-        
-        with open(rendering_path + "\\scene.json", 'w') as fs:
-            fs.write(json.dumps(scene_object))
-            
-      
-        subprocess.run(proc, shell=True, check=False)
-
-        #img = imageio.imread(output_file_path)/255.0
-        #img = np.flip(img, axis=0)
-        #img = img.reshape(self.size_x * self.size_y, 3)
-        #img = np.concatenate((img, np.ones((self.size_x * self.size_y, 1))), axis=1)
-
-        
-        
-        # Here we write the pixel values to the RenderResult
         result = self.begin_result(0, 0, self.size_x, self.size_y)
         layer = result.layers[0].passes["Combined"]
-        #layer.rect = img
+        #layer.rect = rect        
         self.end_result(result)
+
 
     # For viewport renders, this method gets called once at the start and
     # whenever the scene or 3D viewport changes. This method is where data
